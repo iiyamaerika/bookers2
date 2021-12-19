e3b494f9655370dd7cdc67037fa638e2d6b7e63c
EC2 Default User <ec2-user@ip-172-31-16-220.ec2.internal>
2021-12-19 12:52:33 +0000

    [Add]ヘッダー

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,9 @@
 class HomesController < ApplicationController
-  def top
+  def edit
+    @books = Post.find(params[:id])
   end
+  
+  def destroy
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  resources :books, only: [:show, :update, :index, :create]
+  resources :users, only: [:show, :edit, :update, :create, :index]
+  resources :homes, only: [:show, :update]
 end
